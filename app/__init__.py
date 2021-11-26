bb140f17e3b31e51b2d22ca78b46d6a05bdd555b
DianaY-a11y <80307545+DianaY-a11y@users.noreply.github.com>
2021-11-25 23:01:16 -0500

    i

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+import os
+import logging
+
+from flask import Flask, request
+from flask_cors import CORS
+from flask_migrate import Migrate
+from sqlalchemy_utils import create_database, database_exists
+
+
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        record.url = request.url
+        record.remote_addr = request.remote_addr
+        return super().format(record)
+
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    CORS(app)  # add CORS
+
+    db_url = 'sqlite:///main.db'
+    if not database_exists(db_url):
+        create_database(db_url)
+
+    from app.models import db
+
+    db.init_app(app)  # initialize Flask SQLALchemy with this flask app
+    Migrate(app, db)
+
+    return app
