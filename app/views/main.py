63df670a84db88cfb8eb170afe1e971910815267
DianaY-a11y <80307545+DianaY-a11y@users.noreply.github.com>
2021-11-25 23:09:27 -0500

    i

---
+++ app/views/main.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, flash, redirect, render_template, request, session
+from app.models import db 
+from sqlalchemy import inspect
+
+main = Blueprint("main", __name__)  # initialize blueprint
+
+@main.route("/")
+def index():
+    return render_template("index.html")
